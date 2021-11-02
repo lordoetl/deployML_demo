91dc75c6cb52e1c2a2c86d72835adf5c91e22cf6
lordoetl <lordoetl@gmail.com>
2021-11-02 08:32:03 -0500

    All working now

---
+++ app_nn.py
@@ -0,0 +1,70 @@
+# Load libraries
+import flask
+import pandas as pd
+import tensorflow as tf
+import keras
+from keras.models import load_model
+import json
+from json import JSONEncoder
+import numpy
+
+#This function is used to convert the NN prediction to JSON
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+# instantiate flask 
+app = flask.Flask(__name__)
+
+
+
+# define a predict function as an endpoint 
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+
+    #get the message from the POST (Could put test here to verify 
+    # incoming data is correct format i.e. the right number of arguments)
+    params = flask.request.json
+    if (params != None):
+        #create dataframe from data
+        x=pd.DataFrame.from_dict(params)
+        #One Hot Encode
+        x=pd.get_dummies(x)
+        #This was put here to handle extra get-dummies columns
+        # Doing better prework would allow you to skip these next 2 lines
+        model_columns = ['Age', 'Embarked_C', 'Embarked_Q', 'Embarked_S', 'Embarked_nan',
+        'Sex_male'] 
+        # This line will fill any missing column with a 0.  Care should be taken that
+        # won't affect your output
+        x= x.reindex(columns=model_columns, fill_value=0)
+
+        # print ('Model columns loaded')
+        
+        data=model.predict(x)
+
+    # return a response in json format 
+    out=json.dumps(data, cls=NumpyArrayEncoder)
+    return flask.jsonify(out)  
+
+    # The output will be numeric (floats), you may need to build,
+    # a translating function here to make that output meaningful.
+    # for answer in out:
+    #   if answer > .5:
+    #       return ("True")    
+    #   else:
+    #      return("False")
+
+# start the flask app, allow remote connections 
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1]) # This is for a command-line input
+    except:
+        port = 12345 # If you don't provide any port the port will be set to 12345
+
+    model = load_model('example.h5')
+    print ('Model loaded')
+
+
+    app.run(port=port, debug=True)
